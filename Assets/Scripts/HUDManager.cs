1cd8f49b4d69010892810e91137e6afb5a93ecc3
Evenilink <evenilink.code@gmail.com>
2018-10-03 20:16:11 +0100

    Forgot to commit the HUD Manager class. For now, it subcribes to events regarding changes in the suspiciousness level.

---
+++ Assets/Scripts/HUDManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour {
+
+    private Slider suspiciousnessSlider;
+
+	void Start () {
+        suspiciousnessSlider = GetComponentInChildren<Slider>();
+        AlertState.OnSuspiciousnessChange += OnSuspiciousnessChange;
+    }
+	
+    private void OnSuspiciousnessChange(float newValue) {
+        suspiciousnessSlider.value = newValue;
+    }
+}
