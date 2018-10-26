f828dd741723e376f298556fd686270538949c36
Evenilink <evenilink.code@gmail.com>
2018-10-26 16:26:39 +0100

    Weapon gets attached to the hand at the start of the game.

---
+++ Assets/AttachWeapon.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class AttachWeapon : MonoBehaviour {
+
+    [SerializeField] private Transform attachTransform;
+
+    void Start () {
+        transform.parent = attachTransform;
+        transform.localPosition = Vector3.zero;
+	}
+}
